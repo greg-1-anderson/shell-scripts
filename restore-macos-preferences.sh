7a1dcd8b7c42e9cc849dbfdc46662ef8a68668b0
Greg Anderson <greg.1.anderson@greenknowe.org>
2023-06-14 13:13:35 -0700

    Add a script to add and a script to restore Mac system preferences.

---
+++ restore-macos-preferences.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+# Always show scrollbars
+defaults write NSGlobalDomain AppleShowScrollBars -string Always
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# System Preferences > Keyboard >
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# System Preferences > Keyboard >
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# System Preferences > Dock > Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool false
+
+# System Preferences > Accessibility > Mouse & Trackpad
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad -string '{
+    Clicking = 0;
+    DragLock = 0;
+    Dragging = 0;
+    TrackpadCornerSecondaryClick = 0;
+    TrackpadFiveFingerPinchGesture = 2;
+    TrackpadFourFingerHorizSwipeGesture = 2;
+    TrackpadFourFingerPinchGesture = 2;
+    TrackpadFourFingerVertSwipeGesture = 2;
+    TrackpadHandResting = 1;
+    TrackpadHorizScroll = 1;
+    TrackpadMomentumScroll = 1;
+    TrackpadPinch = 1;
+    TrackpadRightClick = 1;
+    TrackpadRotate = 1;
+    TrackpadScroll = 1;
+    TrackpadThreeFingerDrag = 0;
+    TrackpadThreeFingerHorizSwipeGesture = 2;
+    TrackpadThreeFingerTapGesture = 0;
+    TrackpadThreeFingerVertSwipeGesture = 2;
+    TrackpadTwoFingerDoubleTapGesture = 0;
+    TrackpadTwoFingerFromRightEdgeSwipeGesture = 3;
+    USBMouseStopsTrackpad = 0;
+    UserPreferences = 1;
+    version = 5;
+}'
+
+# Finder > View > Show Path Bar
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder > View > Show Path Bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Kill affected apps
+for app in "Dock" "Finder"; do
+  killall "${app}" > /dev/null 2>&1
+done
+
+# Done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
---
+++ save-macos-preferences.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Save a certain subset of MacOS default preference values. Write a script similar to
+ * https://github.com/pawelgrzybek/dotfiles/blob/master/setup-macos.sh to restore settings.
+ * 
+ * Usage:
+ * 
+ * 		php save-macos-preferences.php > restore-macos-preferences.sh
+ * 
+ */
+
+// Properties to save
+$properties = [
+	'NSGlobalDomain AppleShowScrollBars'				=> 'Always show scrollbars',
+	'com.apple.LaunchServices LSQuarantine'				=> 'Disable the “Are you sure you want to open this application?” dialog',
+	'NSGlobalDomain NSAutomaticCapitalizationEnabled'	=> 'Disable automatic capitalization as it’s annoying when typing code',
+	'NSGlobalDomain NSAutomaticDashSubstitutionEnabled'	=> 'Disable smart dashes as they’re annoying when typing code',
+	'NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled'	=> 'Disable automatic period substitution as it’s annoying when typing code',
+	'NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled'	=> 'Disable smart quotes as they’re annoying when typing code',
+	'NSGlobalDomain NSAutomaticSpellingCorrectionEnabled'	=> 'Disable auto-correct',
+
+	'NSGlobalDomain KeyRepeat'							=> 'System Preferences > Keyboard >',
+	'NSGlobalDomain InitialKeyRepeat' 					=> 'System Preferences > Keyboard >',
+
+	'com.apple.dock tilesize'							=> 'System Preferences > Dock > Size',
+	'com.apple.dock magnification'						=> 'System Preferences > Dock > Magnification',
+	'com.apple.dock autohide'							=> 'System Preferences > Dock > Automatically hide and show the Dock',
+	'com.apple.dock show-process-indicators'			=> 'System Preferences > Dock > Show indicators for open applications',
+
+	'com.apple.driver.AppleBluetoothMultitouch.trackpad'	=> 'System Preferences > Accessibility > Mouse & Trackpad',
+
+	'NSGlobalDomain AppleShowAllExtensions' 			=> 'Finder: show all filename extensions',
+	'com.apple.finder ShowPathbar' 						=> 'Finder > View > Show Path Bar',
+	'com.apple.finder _FXSortFoldersFirst'				=> 'Keep folders on top when sorting by name',
+];
+
+print "#!/bin/bash\n\n";
+
+// Read current value of setting and write out a script to restore it
+foreach ($properties as $property => $comment) {
+	$values = [];
+	exec("defaults read $property 2>/dev/null", $values, $status);
+	if ($status == 0) {
+		$type = "-string";
+		$value = implode("\n", $values);
+		if (count($values) > 1) {
+			$value = "'$value'";
+		}
+		if (is_numeric($value)) {
+			$type = "-int";
+			$value = (int)$value;
+		}
+		if (($value === 0) || ($value === 1)) {
+		    $type = "-bool";
+		    $value = $value ? 'true' : 'false';
+		}
+		if (!empty($comment)) {
+			print "\n# $comment\n";
+		}
+		print "defaults write $property $type $value\n";
+	}
+}
+
+// Print script postscript
+print <<< __EOT__
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Kill affected apps
+for app in "Dock" "Finder"; do
+  killall "\${app}" > /dev/null 2>&1
+done
+
+# Done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
+
+__EOT__;
